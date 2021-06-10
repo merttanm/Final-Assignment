c60ab9bd804891a5a45d54e0eccd2158fcebf4ce
merttanm <68250572+merttanm@users.noreply.github.com>
2021-06-10 23:42:22 +0300

    .

--- LogisticAndUserRelations/src/relationssystem/model/Customer.java
+++ LogisticAndUserRelations/src/relationssystem/model/Customer.java
@@ -11,4 +11,77 @@ package relationssystem.model;
  */
 public class Customer {
     
+    private Adress customerAdress;
+    private String customerName;
+    private String customerSurname;
+
+    public Adress getCustomerAdress() {
+        return customerAdress;
+    }
+
+    public void setCustomerAdress(Adress customerAdress) {
+        this.customerAdress = customerAdress;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerSurname() {
+        return customerSurname;
+    }
+
+    public void setCustomerSurname(String customerSurname) {
+        this.customerSurname = customerSurname;
+    }
+
+    public Long getIdentityNo() {
+        return identityNo;
+    }
+
+    public void setIdentityNo(Long identityNo) {
+        this.identityNo = identityNo;
+    }
+
+    public Integer getCustomerAge() {
+        return customerAge;
+    }
+
+    public void setCustomerAge(Integer customerAge) {
+        this.customerAge = customerAge;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isIsValid() {
+        return isValid;
+    }
+
+    public void setIsValid(boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    public int getDriverCarNumber() {
+        return driverCarNumber;
+    }
+
+    public void setDriverCarNumber(int driverCarNumber) {
+        this.driverCarNumber = driverCarNumber;
+    }
+    private Long identityNo;
+    private Integer customerAge;
+    private String phoneNumber;
+    private boolean isValid ; // yaşı 18 den buyukmu  
+    private int driverCarNumber;
+    
 }
--- LogisticAndUserRelations/src/relationssystem/model/Vehicle.java
+++ LogisticAndUserRelations/src/relationssystem/model/Vehicle.java
@@ -11,4 +11,59 @@ package relationssystem.model;
  */
 public class Vehicle {
     
+    private String city;
+    private String district;
+    private String stress;
+    private String apartment;
+    private Integer apartmentId;
+    private Integer doorNo;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getStress() {
+        return stress;
+    }
+
+    public void setStress(String stress) {
+        this.stress = stress;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public Integer getApartmentId() {
+        return apartmentId;
+    }
+
+    public void setApartmentId(Integer apartmentId) {
+        this.apartmentId = apartmentId;
+    }
+
+    public Integer getDoorNo() {
+        return doorNo;
+    }
+
+    public void setDoorNo(Integer doorNo) {
+        this.doorNo = doorNo;
+    }
+    
 }
