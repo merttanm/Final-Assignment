b887a580e73f5e7720c685c146e015d67ff70df0
merttanm <68250572+merttanm@users.noreply.github.com>
2021-06-20 20:53:06 +0300

    Controller ve View işlemleri

--- LogisticAndUserRelations/src/controller/LogisticAndUserRelations.java
+++ LogisticAndUserRelations/src/controller/LogisticAndUserRelations.java
@@ -5,17 +5,104 @@
  */
 package controller;
 
+import relationssystem.model.Car;
+import relationssystem.model.Contact;
+import relationssystem.model.Customer;
+import view.CarView;
+import view.ContactView;
+import view.CustomerView;
+import view.FreightCompanyView;
+
 /**
  *
  * @author MERT
  */
 public class LogisticAndUserRelations {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private Car car;
+    private Customer customer;
+    private Contact contact;
+
+    private CarView carView;
+    private CustomerView customerView;
+    private ContactView contactView;
+    private FreightCompanyView reservationView;
+
+    public LogisticAndUserRelations(Car car, CarView carView) {
+        this.car = car;
+        this.carView = carView;
     }
-    
+
+    public LogisticAndUserRelations(CustomerView customerView) {
+        this.customerView = customerView;
+    }
+
+    public LogisticAndUserRelations(Contact contact, ContactView contactView) {
+        this.contact = contact;
+        this.contactView = contactView;
+    }
+
+    public LogisticAndUserRelations() {
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Contact getReservation() {
+        return contact;
+    }
+
+    public void setReservation(Contact contact) {
+        this.contact = contact;
+    }
+
+    public CarView getCarView() {
+        return carView;
+    }
+
+    public void setCarView(CarView carView) {
+        this.carView = carView;
+    }
+
+    public CustomerView getCustomerView() {
+        return customerView;
+    }
+
+    public void setCustomerView(CustomerView customerView) {
+        this.customerView = customerView;
+    }
+
+    public ContactView getReservationView() {
+        return contactView;
+    }
+
+    public void setReservationView(ContactView contactView) {
+        this.contactView = contactView;
+    }
+
+    public void updateCarView() {
+        carView.printCarDetail(car);
+    }
+
+    public void updateCustomerView() {
+        customerView.printCustomerDetail(customer);
+    }
+
+    public void updateRezervaionView() {
+        contactView.printContactDetailView(contact);
+    }
+
 }
--- LogisticAndUserRelations/src/view/ContactView.java
+++ LogisticAndUserRelations/src/view/ContactView.java
@@ -5,10 +5,33 @@
  */
 package view;
 
+import relationssystem.model.Car;
+import relationssystem.model.Contact;
+
 /**
  *
  * @author MERT
  */
 public class ContactView {
-    
+   
+    public void printReservationDetailView(Contact contact){
+        System.out.println("REZEVASYON BİLGİLERİ *************");
+        System.out.println("REZERVASYON YAPAN MÜŞTERİ BİLGİLERİ *****");
+        System.out.println("       ADI : " + contact.getCustomer().getCustomerName());
+        System.out.println("    SOYADI : " + contact.getCustomer().getCustomerSurname());
+        System.out.println("    TEL NO : " + contact.getCustomer().getPhoneNumber());
+        
+        System.out.println("REZERV EDİLEN ARAÇ BİLGİLERİ ********");
+        for(Car car: contact.getCarList())
+        {
+            System.out.println("    ARAÇ MARKA   :"  + car.getName());
+            System.out.println("    ARAÇ MODEL   :"  + car.getModel());
+            System.out.println("    ARAÇ RENK    :"  + car.getColor());
+            System.out.println("    ARAÇ PLAKA   :"  +car.getPlate());
+            System.out.println("    ARAÇ DURUM   :"  + (car.isCarReserved()==true ? "ARAÇ REZERVELİ. " : "ARAÇ REZERVİ BİTTİ"));
+            System.out.println("    REZERV TUTAR :" + car.calcRentPrice());
+        }
+    }
 }
+    
+
